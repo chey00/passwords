1c37016f23237ed5216c2a49549d5876de5c08a7
Christian Hey <c.hey@sbs-herzogenaurach.de>
2023-12-04 13:23:53 +0100

    Übung 2

--- gehalt.py
+++ gehalt.py
@@ -1,10 +1,11 @@
+# 2) Erweitern Sie das Programm wiefolgt:
+#    Programmieren Sie eine Funktion add_employee. Die Funktion erhält als Prameter das
+#    dict employee_and_salery. In das Dictonary wird ein neuer Mitarbeitername und das Gehalt eingefügt.
+#    Zuerst prüft es aber, ob der Mitarbeitername schon vergeben ist.
+
 # Erstellen Sie ein Dictionary mit Mitarbeitern und Gehalt.
 
 employee_and_salery = dict()
 
-print(employee_and_salery)
-
 employee_and_salery["Franz Mayer"] = 2123.76
-employee_and_salery["Frank Petersen"] = 3421.45
-
-print(employee_and_salery)
+employee_and_salery["Hans Petersen"] = 3421.45
