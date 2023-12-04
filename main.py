575f64e2c17650f3ff7bb910773b4b426235a943
Christian Hey <c.hey@sbs-herzogenaurach.de>
2023-12-04 13:19:10 +0100

    Lösüung für Übung 1

--- main.py
+++ main.py
@@ -9,12 +9,31 @@ def check_login(names_and_passwords):
 
 # 1) Erstellen Sie eine Methode, welche Benutzer hinzufügt. Beachten Sie, dass ein Benutzername nicht doppelt vergeben
 #    werden darf. Der Methodenname ist add_user mit names_and_passwords als dict.
+def add_user(names_and_passwords):
+    username = input("Geben Sie den neuen Benutzernamen ein: ")
+
+    usernames = names_and_passwords.keys()
+
+    if username in usernames:
+        print("Benutername schon vergeben")
+
+        return False
+    else:
+        password = input("Geben Sie das neue Passwort ein: ")
+
+        names_and_passwords[username] = password
+
+        return True
+
 
 dictionary = { "hans": "ABCD1234",
                "peter": "ABCD1234",
                "root": "2t23$&fdsgeFJZ"}
 
+add_user(dictionary)
+'''
 while True:
     if check_login(dictionary):
         print("Login erfolgreich!")
         break
+'''
